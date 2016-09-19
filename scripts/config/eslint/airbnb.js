e0e3030c385386b8e596f7d9192d83a166418bbf
Michal Kvasničák <michal.kvasnicak@gmail.com>
2016-09-19 17:44:44 +0200

    make airbnb eslint configwork with async functions

--- scripts/config/eslint/airbnb.js
+++ scripts/config/eslint/airbnb.js
@@ -1,14 +1,36 @@
 module.exports = {
   root: true,
+  parser: 'babel-eslint',
   env: {
-    browser: true
+    browser: true,
+    commonjs: true,
+    es6: true,
+    node: true
   },
   extends: [
     require.resolve('eslint-config-airbnb')
   ],
+  parserOptions: {
+    ecmaVersion: 2016,
+    sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true,
+      generators: true,
+      experimentalObjectRestSpread: true,
+    },
+  },
   plugins: ['flowtype'],
   rules: {
-    'import/*': 0,
-    'react/require-extension': 0
+    'generator-star-spacing': 'off',
+    'import/*': 'off',
+    'react/require-extension': 'off',
+    'flowtype/define-flow-type': 'warn',
+    'flowtype/require-valid-file-annotation': 'warn',
+    'flowtype/use-flow-type': 'warn'
+  },
+  "settings": {
+    "flowtype": {
+      "onlyFilesWithFlowAnnotation": false
+    }
   }
 };
