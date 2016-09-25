e303ea7c8523367c31e9c6cb76fc10a2b647ee5c
Michal Kvasničák <michal.kvasnicak@gmail.com>
2016-09-25 14:38:50 +0200

    add eslint-config-js-app package

---
+++ packages/eslint-config-js-app/README.md
@@ -0,0 +1,3 @@
+# eslint-config-js-app
+
+Default shareable eslint configuration used by [Create JS App](https://github.com/michalkvasnicak/create-js-app)
---
+++ packages/eslint-config-js-app/index.js
@@ -0,0 +1,37 @@
+const importPluginRules = Object.keys(require('eslint-plugin-import').rules);
+const airbnbConfig = Object.assign(
+  {},
+  require('eslint-config-airbnb'),
+  {
+    root: true,
+    env: {
+      browser: true,
+      commonjs: true,
+      es6: true,
+      jest: true,
+      node: true,
+    },
+    parserOptions: {
+      sourceType: 'module',
+      ecmaFeatures: {
+        generators: true,
+        experimentalObjectRestSpread: true,
+      },
+    },
+    plugins: ['flowtype', 'flowtype-errors'],
+    // disable import rules temporarily
+    rules: importPluginRules.reduce(
+      (rules, rule) => Object.assign({}, rules, { [`import/${rule}`]: 'off' }), {}
+    ),
+    settings: {
+      flowtype: {
+        onlyFilesWithFlowAnnotation: true,
+      },
+    },
+  }
+);
+
+// add flowtype recommended configuration
+airbnbConfig.extends.push('plugin:flowtype/recommended');
+
+module.exports = airbnbConfig;
