7b2c8e4f39f8f3e98d1cca27d6288f1b60c87b33
Michal Kvasničák <michal.kvasnicak@gmail.com>
2016-09-25 15:28:11 +0200

    add create-js-app-scripts binary

---
+++ packages/create-js-app-scripts/bin/create-js-app-scripts.js
@@ -0,0 +1,25 @@
+#!/usr/bin/env node
+const chalk = require('chalk');
+const spawn = require('cross-spawn');
+const script = process.argv[2];
+const args = process.argv.slice(3);
+
+switch (script) {
+  // case 'build':
+  // case 'eject':
+  case 'start': {
+    // case 'test':
+    const result = spawn.sync(
+      'node',
+      [require.resolve(`../build/${script}`)].concat(args),
+      { stdio: 'inherit' }
+    );
+    process.exit(result.status);
+    break;
+  }
+  default:
+    console.log(chalk.red(`Unknown script '${script}'.`));
+    console.log(chalk.red('Perhaps you need to update react-scripts?'));
+    process.exit(1);
+    break;
+}
