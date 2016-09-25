6a03fe38426b99740c6d4eca7ce38f2c12dc17fc
Michal Kvasničák <michal.kvasnicak@gmail.com>
2016-09-25 14:38:29 +0200

    cleanup old code

--- scripts/config/webpack/client.config.js
+++
@@ -1,7 +0,0 @@
-/* @flow */
-const webpackConfigFactory = require('./configFactory');
-
-module.exports = function clientConfigFactory(options /*: Object */ = {}, args /*: Object */ = {}) /*: Object */ {
-  const { mode = 'development' } = options;
-  return webpackConfigFactory({ target: 'client', mode }, args);
-};
--- scripts/config/webpack/server.config.js
+++
@@ -1,7 +0,0 @@
-/* @flow */
-const webpackConfigFactory = require('./configFactory');
-
-module.exports = function serverConfigFactory(options /*: Object */ = {}, args /*: Object */ = {}) /*: Object */ {
-  const { mode = 'development' } = options;
-  return webpackConfigFactory({ target: 'server', mode }, args);
-};
