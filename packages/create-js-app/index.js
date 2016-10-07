a08fe0e0f860197fb88ecf7cd8622f2bb3bccfbd
Michal Kvasničák <michal.kvasnicak@gmail.com>
2016-10-07 13:12:56 +0200

    :bug: fix create command

--- packages/create-js-app/index.js
+++ packages/create-js-app/index.js
@@ -2,10 +2,11 @@
 /* eslint-disable no-unused-expressions, no-console, global-require */
 
 const yargs = require('yargs');
+const createCommand = require('./commands/create');
 
 yargs
 .usage('Usage: $0 <cmd> [args]')
-.commandDir('commands')
+.command(createCommand)
 .help()
 .version('version', 'display version information', require('./package.json').version)
 .argv;
