8a7cd70e5dc40e543e451f282671c9d7290172c6
Michal Kvasničák <michal.kvasnicak@gmail.com>
2016-10-14 10:30:40 +0200

    :bug: fix arrow-parens linting when flow annotations are used

--- packages/eslint-config-js-app/index.js
+++ packages/eslint-config-js-app/index.js
@@ -22,7 +22,7 @@ const airbnbConfig = Object.assign(
     },
     // temporarily disable flowtype-errors plugin
     // because it is messing with RAM
-    plugins: ['flowtype'/* , 'flowtype-errors'*/],
+    plugins: ['babel', 'flowtype'/* , 'flowtype-errors'*/],
     // disable import rules temporarily
     rules: Object.assign(
       {},
@@ -30,6 +30,10 @@ const airbnbConfig = Object.assign(
         (rules, rule) => Object.assign({}, rules, { [`import/${rule}`]: 'off' }), {}
       ),
       {
+        'arrow-parens': 'off',
+        'babel/arrow-parens': ['error', 'as-needed', {
+          requireForBlockBody: true,
+        }],
         // 'flowtype-errors/show-errors': 'error', todo enable when problems are solved
         'new-cap': 'off',
         'react/jsx-filename-extension': 'off',
