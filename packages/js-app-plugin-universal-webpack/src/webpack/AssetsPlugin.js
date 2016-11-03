0edee542378cb3dd5fa7f50d26cfd13ff2caed94
Michal Kvasničák <michal.kvasnicak@gmail.com>
2016-11-03 12:38:47 +0100

    :bug: topologically sort assets

--- packages/js-app-plugin-universal-webpack/src/webpack/AssetsPlugin.js
+++ packages/js-app-plugin-universal-webpack/src/webpack/AssetsPlugin.js
@@ -2,6 +2,7 @@
 const fs = require('fs');
 const fsExtra = require('fs-extra');
 const path = require('path');
+const toposort = require('toposort');
 
 module.exports = class AssetsPlugin {
   assetsPath: string;
@@ -17,35 +18,53 @@ module.exports = class AssetsPlugin {
       const publicPath = compiler.options.output.publicPath;
 
       const json = stats.toJson();
+      const initialChunks = json.chunks.filter(chunk => chunk.initial);
+      const chunks = {};
       const entryPoints = {};
 
-      json
-        .chunks
-        .reverse()
-        .filter(chunk => chunk.initial)
-        .forEach((chunk) => {
-          let map;
+      initialChunks.forEach((chunk) => {
+        chunks[chunk.id] = chunk;
+      });
 
-          if (entryPoints[chunk.names[0]]) {
-            map = entryPoints[chunk.names[0]];
-          } else {
-            map = entryPoints[chunk.names[0]] = { css: [], js: [] };
-          }
+      const edges = [];
 
-          const files = Array.isArray(chunk.files) ? chunk.files : [chunk.files];
-
-          files.forEach((file) => {
-            const filePath = publicPath + file;
-
-            if (/\.js$/.test(file)) {
-              map.js.push(filePath);
-            } else if (/\.css$/.test(file)) {
-              map.css.push(filePath);
+      initialChunks.forEach((chunk) => {
+        if (chunk.parents) {
+          // Add an edge for each parent (parent -> child)
+          chunk.parents.forEach((parentId) => {
+            const parentChunk = chunks[parentId];
+            // If the parent chunk does not exist (e.g. because of an excluded chunk)
+            // we ignore that parent
+            if (parentChunk) {
+              edges.push([parentChunk, chunk]);
             }
           });
+        }
+      });
 
-          return entryPoints;
-        }, {});
+      const sorted = toposort.array(initialChunks, edges);
+
+      sorted.forEach((chunk) => {
+        let map;
+
+        if (!entryPoints[chunk.names[0]]) {
+          map = entryPoints[chunk.names[0]] = { css: [], js: [] };
+        } else {
+          map = entryPoints[chunk.names[0]];
+        }
+
+        const files = Array.isArray(chunk.files) ? chunk.files : [chunk.files];
+
+        files.forEach((file) => {
+          const filePath = publicPath + file;
+
+          if (/\.js$/.test(file)) {
+            map.js.push(filePath);
+          } else if (/\.css$/.test(file)) {
+            map.css.push(filePath);
+          }
+        });
+      });
 
       // create build directory just in case
       fsExtra.mkdirsSync(path.dirname(this.assetsPath));
