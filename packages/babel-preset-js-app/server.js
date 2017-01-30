615d0e151281c85cbe370c8ca4c647c1a449822f
Michal Kvasničák <michal.kvasnicak@gmail.com>
2017-01-30 13:12:56 +0100

    :bug: add missing babel-plugin-transform-class-properties

--- packages/babel-preset-js-app/server.js
+++ packages/babel-preset-js-app/server.js
@@ -16,6 +16,8 @@ module.exports = {
     require.resolve('babel-preset-react'),
   ],
   plugins: [
+    // class { handleClick = () => { } }
+    require.resolve('babel-plugin-transform-class-properties'),
     // { ...param, completed: true }
     require.resolve('babel-plugin-transform-object-rest-spread'),
   ],
